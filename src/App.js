72a59fd3f3481548470bf8da5f4713dd5bf3c376
Robb Doering <robert.w.doering@vanderbilt.edu>
2021-07-01 16:50:56 -0600

    started on adding resume data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { Card } from 'semantic-ui-react';
+
+import { projects, experience, skills, education } from './constants';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default props => {
+	return (
+		<div className='app-root'>
+			<div className='info-container'>
+				<div className='name-container'>
+					<h1> Robb Doering </h1>
+					<h2> is a fullstack software engineer looking for work. </h2>
+				</div>
+			</div>
+			<div className='details-container'>
+				<div className='projects-container'>
+					<Card header='Origami Odyssey'>
 
-export default App;
+					</Card>	
+
+					<Card header='Bellum.ai'>
+					</Card>	
+
+					<Card header='Data Viz in VR'>
+					</Card>	
+				</div>
+
+				<div className='experience-container'></div>
+
+				<div className='skills-container'></div>
+
+				<div className='education-container'></div>
+			</div>
+		</div>
+	);
+};
---
+++ src/constants.js
@@ -0,0 +1,282 @@
+export const employment = [
+	{
+		title: 'Software Engineer II',
+		company: 'Space Ground Systems Solutions',
+		dates: 'June 2019 - July 2021',
+		bullets: [
+			'Worked on a web application for space-ground command & control systems, using React w/ Redux, Java, and C++ to display & interact with high latency,  error sensitive ground site information',
+			'Chosen to fill in as project lead during a 3-month planned leave of absence for a team of 6 other full time engineers. Responsible for leading Agile ceremonies, clearing blockers, meeting with stakeholders, UX and system design for new features, designing and delivering training, and more.',
+			'Implemented ~12 significant full stack features, using description documents from software architects for some, and leading the requirements gathering and UX design processes myself for others.',
+			'Owned implementation of systems used by many government satellite programs, contributing to an essential piece of the Naval Research Laboratory’s response to specific DoD-wide standardization and modernization mandates for space related software.',
+			'Regularly and deeply involved in testing, such as being tapped to bring Jest unit tests to an existing code base, or design and lead implementation of plans for functional testing.',
+		]
+	}
+];
+
+export const projects = [
+	{
+		name: 'Origami Odyssey',
+		link: 'https://github.com/robbwdoering/origamiodyssey',
+		sourceLink: 'https://origamiodyssey.herokuapp.com/',
+		summary: 'A React SPA built to teach origami, the art of folding paper. It uses animated 3D simulations with temporal & spatial controls, hierarchical annotations of folds, and hobbyist augmentations to improve learner performance on audience specific self-assessments. Each specific method in that sentence is supported by research I did as part of GaTech’s CS6460: Educational Technology.',
+		technologies: [
+			'Javascript', 'React', 'Redux', 'Three.js', 'react-three-fiber', 'Material-UI'
+		]
+	}
+];
+
+export const skills = [
+	{
+		name: 'Linux'
+		desc: 'I first learned linux in undergrad, use it every day professionally, and do all my personal projects in Ubuntu VM\'s. I have successfully debugged many environment issues in these contexts, and I am comfortable with reasonably advanced CLI patterns, such as backtics, pipes, and bespoke script creation.',
+		level: 'Advanced',
+		category: 'languages'
+	},
+	{
+		name: 'agile'
+		desc: 'I have professional experience both participating in and leading sprint ceremonies, and have discussed the logic behind the system as part of my courses at Georgia Tech.',
+		level: 'Advanced',
+		category: 'concepts'
+	},
+	{
+		name: 'git'
+		desc: 'I have used git extensively for professional & academic collaboration, and for version control on personal projects. This includes managing and reviewing pull requests, working with patch and release branches, using git submodules, and debugging branch conflicts for myself and others.',
+		level: 'Advanced',
+		category: 'tools'
+	},
+	{
+		name: 'Java'
+		desc: 'I first learned Java in undergrad, and now have professional experience with Java web servers in multiple roles, both using the Spring framework and a simpler dependency-manager solution. I have successfully debugged many buildtime issues with the Java classpath, and have experience building Java systems using Gradle and Maven.',
+		level: 'Advanced',
+		category: 'languages'
+	},
+	{
+		name: 'Javascript'
+		desc: 'Javascript is currently the language I\'m most comfortable with, both older JQuery-based systems and more modern ES6 syntax. I greatly appreciate the speed of iteration that the language allows for in the right contexts, and have extensive experience with the various methods that make up modern JS (i.e. map/reduce, import/export, JSON manipulation methods, etc.).',
+		level: 'Advanced',
+		category: 'languages'
+	},
+	{
+		name: 'React'
+		desc: 'I have extensive experience creating complex Single Page Applications using React, and combining it with various other libraries and frameworks. Most of my professional frontend experience is in React, along with all of my personal projects (including this site).',
+		level: 'Advanced',
+		category: 'libraries'
+	},
+	{
+		name: 'Jest'
+		desc: 'I was partially responsible for the initial implementation of Jest tests for a large existing codebase, and am used to maintaining coverage threshold targets over the long term. I am a big believer in the utility of Jest for unit and regression testing, and although I have no professional experience with Test Driven Development, I would relish the opportunity to try it.',
+		level: 'Advanced',
+		category: 'libraries'
+	},
+	{
+		name: 'Redux'
+		desc: 'All of the more complex React applications I have worked on use Redux for state management, and I am comfortable writing new reducers and fields within the Redux store from scratch. I feel I have a solid understanding of how the systems works conceptually, both from a MOOC on the topic and from professional experience.',
+		level: 'Advanced',
+		category: 'libraries'
+	},
+	{
+		name: 'C'
+		desc: 'I have used C professionally as part of full stack web applications, and academically for lessons on IPC protocols, performant computing, and Operation System design. I also have experience writing, reading, and maintaining complex Makefiles.',
+		level: 'Advanced',
+		category: 'languages'
+	},
+	{
+		name: 'C++'
+		desc: 'I have used C++ professionally as part of full stack web applications, and academically for lessons on data structures, standard library usage, and software design patterns. I also have experience writing, reading, and maintaining complex Makefiles.',
+		level: 'Advanced',
+		category: 'languages'
+	},
+	{
+		name: 'CSS'
+		desc: 'I have used CSS extensively professionally and for personal projects, and feel comfortable with a variety of selectors and rules. I have tried vanilla CSS and CSS-as-code (read: CSS defined in JS), but prefer the former. I have not used any CSS frameworks, but would relish the opportunity.',
+		level: 'Advanced',
+		category: 'languages'
+	},
+	{
+		name: 'HTML'
+		desc: 'I have designed sites using raw HTML professionally, and although all of my more recent experience manipulates the DOM indirectly using React, I feel comfortable with the underlying concepts.',
+		level: 'Advanced',
+		category: 'languages'
+	},
+	{
+		name: 'npm'
+		desc: 'I have used npm professionally and for personal projects, and have experience comparing and contrasting options in terms of security, stability, and long term maintainability in a formal context.',
+		level: 'Advanced',
+		category: 'tools'
+	},
+	{
+		name: 'Three.js'
+		desc: '',
+		level: 'Advanced',
+		category: 'libraries'
+	},
+	{
+		name: 'regex'
+		desc: '',
+		level: 'Advanced',
+		category: 'libraries'
+	},
+	{
+		name: 'JSON'
+		desc: '',
+		level: 'Advanced',
+		category: 'libraries'
+	},
+	{
+		name: 'Slack'
+		desc: '',
+		level: 'Advanced',
+		category: 'tools'
+	},
+	{
+		name: 'Jira'
+		desc: '',
+		level: 'Advanced',
+		category: 'tools'
+	},
+	{
+		name: 'Inkscape'
+		desc: '',
+		level: 'Advanced',
+		category: 'tools'
+	},
+	{
+		name: 'HCI'
+		desc: '',
+		level: 'Advanced',
+		category: 'tools'
+	},
+	{
+		name: 'Python'
+		desc: '',
+		level: 'Functional',
+		category: 'languages'
+	},
+	{
+		name: 'Bash'
+		desc: '',
+		level: 'Functional',
+		category: 'tools'
+	},
+	{
+		name: 'MySQL'
+		desc: '',
+		level: 'Functional',
+		category: 'libraries'
+	},
+	{
+		name: 'PostgreSQL'
+		desc: '',
+		level: 'Functional',
+		category: 'libraries'
+	},
+	{
+		name: 'Node.js'
+		desc: '',
+		level: 'Functional',
+		category: 'libraries'
+	},
+	{
+		name: 'Express.js'
+		desc: '',
+		level: 'Functional',
+		category: 'libraries'
+	},
+	{
+		name: 'D3.js'
+		desc: '',
+		level: 'Functional',
+		category: 'libraries'
+	},
+	{
+		name: 'C#'
+		desc: '',
+		level: 'Functional',
+		category: 'languages'
+	},
+	{
+		name: 'REST'
+		desc: '',
+		level: 'Advanced',
+		category: 'libraries'
+	},
+	{
+		name: 'WebSockets'
+		desc: '',
+		level: 'Advanced',
+		category: 'libraries'
+	},
+	{
+		name: 'satellite.js'
+		desc: '',
+		level: 'Functional',
+		category: 'libraries'
+	},
+	{
+		name: 'GMSEC'
+		desc: '',
+		level: 'Functional',
+		category: 'libraries'
+	},
+	{
+		name: 'Orbitology'
+		desc: '',
+		level: 'Functional',
+		category: 'concepts'
+	},
+	{
+		name: 'TLEs'
+		desc: '',
+		level: 'Functional',
+		category: 'concepts'
+	},
+	{
+		name: 'Unity'
+		desc: '',
+		level: 'Functional',
+		category: 'tools'
+	},
+	{
+		name: 'Lisp'
+		desc: '',
+		level: 'Functional',
+		category: 'languages'
+	},
+	{
+		name: 'R'
+		desc: '',
+		level: 'Functional',
+		category: 'languages'
+	},
+	{
+		name: 'RST'
+		desc: '',
+		level: 'Advanced',
+		category: 'languages'
+	},
+	{
+		name: 'Docker'
+		desc: '',
+		level: 'Functional',
+		category: 'tools'
+	},
+	{
+		name: 'Podman'
+		desc: '',
+		level: 'Functional',
+		category: 'tools'
+	},
+	{
+		name: 'MATLAB'
+		desc: '',
+		level: 'Functional',
+		category: 'languages'
+	},
+
+
+
+];
+
+export const education = [
+
+];
